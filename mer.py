c1dccc6497f843d6e75625eda2282f4c168f12ee
Gusbenz <ryan.casalino@gmail.com>
2017-01-07 18:32:49 -0500

    new auth works, but need way for user input.

--- mer.py
+++ mer.py
@@ -13,101 +13,51 @@ import os.path
 import argparse
 import random
 import requests
+from requests.auth import AuthBase
 from clint.textui import colored
 
-user_name = 'Gusbenz'
 base_url = 'https://api.github.com/'
 token = os.environ.get('GITHUB_API_KEY')
-headers = {'Authorization': 'token %s' % token}
 
 # replace with class for session handling?
 # http://stackoverflow.com/questions/17622439/how-to-use-github-api-token-in-python-for-requesting
 # ##
-class User(object):
+class GitHub_Auth(AuthBase):
   def __init__(self, username, token):
     self.username = username
     self.token = token
 
-class GitHub(object):
-  def __init__(self, arg):
-    self.arg = arg
-    self.session = requests.Session()
-    req = self.session.get(base_url + 'user')
-    try:
-      self.session.headers['Authorization'] = 'token %s' % arg.token
-      print(colored.green('You are authenticated ✔︎'))
-    except AttributeError:
-      print(colored.red('You are not authenticated ✔︎'))
+  def __call__(self, r):
+    r.headers['Authorization'] = 'token %s' % self.token
+    return r
 
-  def get_user():
-    try:
-      magic_num = random.randint(1, 10987)
-      u = self.session.get(base_url + 'users?since=' +
-                       str(magic_num))
-      users = u.json()
-      user = users[0]['login']
-      return user
-    except KeyError:
-      error = users['message']
-      return colored.red('OH SHIT: %s' % error)
+def get_commit(token):
 
-  def get_commit(self, user):
-    user = get_user()
+    magic_num = random.randint(1, 10987)
+    u = requests.get(base_url + 'users?since=' +
+                      str(magic_num), auth=GitHub_Auth('Gusbenz', token))
+    users = u.json()
+    user = users[0]['login']
 
     # get the repo based on user
-    r = self.session.get(base_url + 'users/' +
-                     user + '/repos')
+    r = requests.get(base_url + 'users/' +
+                     user + '/repos', auth=GitHub_Auth('Gusbenz', token))
     repos = r.json()
     repo = repos[0]['name']
 
     # get the commit based on repo
-    c = self.session.get(base_url + 'repos/' +
-                     user + '/' + repo + '/commits')
+    c = requests.get(base_url + 'repos/' +
+                     user + '/' + repo + '/commits', auth=GitHub_Auth('Gusbenz', token))
     commits = c.json()
     commit = commits[0]['sha']
 
     # get the commit message based on the sha of commit
-    m = self.session.get(base_url + 'repos/' +
-                     user + '/' + repo + '/commits/' + str(commit))
+    m = requests.get(base_url + 'repos/' +
+                     user + '/' + repo + '/commits/' + str(commit), auth=GitHub_Auth('Gusbenz', token))
     messages = m.json()
     message = messages['commit']['message']
 
     print(message)
-
-def get_user():
-  try:
-    magic_num = random.randint(1, 10987)
-    u = requests.get(base_url + 'users?since=' +
-                     str(magic_num), headers=headers)
-    users = u.json()
-    user = users[0]['login']
-    return user
-  except KeyError:
-    error = users['message']
-    return colored.red('OH SHIT: %s' % error)
-
-
-def get_commit(user):
-  user = user()
-  # get the repo based on user
-  r = requests.get(base_url + 'users/' +
-                   user + '/repos', headers=headers)
-  repos = r.json()
-  repo = repos[0]['name']
-
-  # get the commit based on repo
-  c = requests.get(base_url + 'repos/' +
-                   user + '/' + repo + '/commits', headers=headers)
-  commits = c.json()
-  commit = commits[0]['sha']
-
-  # get the commit message based on the sha of commit
-  m = requests.get(base_url + 'repos/' +
-                   user + '/' + repo + '/commits/' + str(commit), headers=headers)
-  messages = m.json()
-  message = messages['commit']['message']
-
-  print(message)
 
 #--CLI--#
 
@@ -125,8 +75,7 @@ if __name__ == '__main__':
   args = cli.parse_args()
   if args.authenticate:
     try:
-      player = User('Gusbenz', token)
-      game = GitHub(player)
+      get_commit(token)
     except:
       raise
   if args.commit:
